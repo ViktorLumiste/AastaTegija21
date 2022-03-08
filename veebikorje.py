96ecf8f7cdb0cdab0ffa546491a852e97e71fd2a
Viktor Lumiste <viktor.lumiste@khk.ee>
2022-03-08 13:05:49 +0200

    Menuu andmete listimine valmis

--- veebikorje.py
+++ veebikorje.py
@@ -1,5 +1,25 @@
 import requests
+import json
 from html.parser import HTMLParser
 from bs4 import BeautifulSoup
-url = "http://192.168.22.172/menu-example/"
+import html5lib
+url = "https://siseveeb.voco.ee/veebivormid/sookla_menuu"
 resp = requests.get(url)
+soup = BeautifulSoup(resp.content, 'html5lib')
+class MyHTMLParser(HTMLParser):
+    def handle_data(self, data):
+        if str(data).strip() != "":
+            print("Encountered some data  :", data)
+        else:
+            pass
+naidis = {
+                "nimetus": "nim",
+                "hind": "3.23",
+                "lisainfo": "Väga hea"
+            }
+
+parser = MyHTMLParser()
+parser.feed(str(soup))
+with open("json kasitsi.py", "r") as file:
+    data = json.load(file)
+data.append(entry)
