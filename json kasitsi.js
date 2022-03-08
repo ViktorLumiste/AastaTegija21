32c176b77a5a410df2977c22e314df2e43a00e94
Viktor Lumiste <viktor.lumiste@khk.ee>
2022-03-08 10:12:31 +0200

    Käsitsi json faili kirjutamine

---
+++ json kasitsi.js
@@ -0,0 +1,31 @@
+[
+    {
+        "nimetus":"PRAED",
+        "toidud":[
+            {
+                "nimetus":"Toidu nimetus",
+                "hind":"Toidu hind",
+                "lisainfo":"Täpsustav info toidu kohta - võib olla ka tühjana"
+            }
+        ]
+    },
+    {
+        "nimetus":"SUPID",
+        "toidud":[
+            {
+                "nimetus":"Toidu nimetus",
+                "hind":"Toidu hind",
+                "lisainfo":"Täpsustav info toidu kohta - võib olla ka tühjana"
+            }
+        ]
+    },
+    {
+        "nimetus":"JOOGID",
+        "toidud":[
+            {
+                "nimetus":"Toidu nimetus",
+                "hind":"Toidu hind",
+                "lisainfo":"Täpsustav info toidu kohta - võib olla ka tühjana"
+            }
+        ]
+    }]
