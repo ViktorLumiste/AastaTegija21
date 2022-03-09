790b88566e5130c31f33885ddf10e232383486c8
Viktor Lumiste <viktor.lumiste@khk.ee>
2022-03-09 11:04:01 +0200

    printimisega probleemid aga aeg otsas

---
+++ json mite enam kasitsi.py
@@ -0,0 +1,92 @@
+[
+   {
+       "nimetus": "PRAED",
+       "toidud": [
+            {
+                "nimetus": "Bitkiid",
+                "hind": "3,40€",
+                "lisainfo": "Bitkiid 2tk, lisand, kaste, salat, leib",
+            },
+            {
+                "nimetus": "Kanapall peekoniga",
+                "hind": "3,40€",
+                "lisainfo": "Kanapall, liand, kaste, salat",
+            },
+            {
+                "nimetus": "Sealihašnitsel (L)",
+                "hind": "3,40€",
+                "lisainfo": "Sealihašnitsel, lisand, kaste, salat, leib",
+            },
+            {
+                "nimetus": "Bitkiid VOCO ÕPILASELE",
+                "hind": "2,94€",
+                "lisainfo": "Bitkiid 2tk, lisand, kaste, salat, leib",
+            },
+            {
+                "nimetus": "Kartulikotlet 2tk külma kastmega T",
+                "hind": "2,94€",
+                "lisainfo": "lisand, kaste, salat, leib",
+            },
+            {
+                "nimetus": "Köögiviljapada seentega V, G, L",
+                "hind": "2,94€",
+                "lisainfo": "Pada, salat",
+            },
+       ]
+   },
+   {
+       "nimetus": "SUPID",
+       "toidud": [
+            {
+                "nimetus": "Kodune seljanka (L)",
+                "hind": "1,60€",
+                "lisainfo": "supp, hapukoor, leib",
+            },
+            {
+                "nimetus": "Kanasupp",
+                "hind": "1,40€",
+                "lisainfo": "Supp,leib",
+            },
+       ]
+   },
+   {
+       "nimetus": "MAGUSTOIDUD",
+       "toidud": [
+            {
+                "nimetus": "Kamavaht",
+                "hind": "1,40€",
+                "lisainfo": "",
+            },
+            {
+                "nimetus": "Leivasupp piimaga",
+                "hind": "1,40€",
+                "lisainfo": "",
+            },
+       ]
+   },
+   {
+       "nimetus": "JOOGID",
+       "toidud": [
+            {
+                "nimetus": "Mahl",
+                "hind": "0,61€",
+                "lisainfo": "",
+            },
+            {
+                "nimetus": "Morss",
+                "hind": "0,26€",
+                "lisainfo": "",
+            },
+            {
+                "nimetus": "Piim",
+                "hind": "0,30€",
+                "lisainfo": "",
+            },
+            {
+                "nimetus": "Koolipiim",
+                "hind": "0,20€",
+                "lisainfo": "",
+            },
+      ]
+   }
+]
--- veebikorje.py
+++ veebikorje.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import os
 from html.parser import HTMLParser
 from bs4 import BeautifulSoup
 import re
@@ -10,24 +11,24 @@ url = "https://siseveeb.voco.ee/veebivormid/sookla_menuu"
 resp = requests.get(url)
 soup = BeautifulSoup(resp.content, 'html5lib')
 kood = str(soup)
-kood = kood[7279:]
+kood = kood[7350:]
 menuu = kood
 for i in range(4):
     repl = ['<h3><strong>', '</strong></h3>', '<li class="list-group-item"><h2>', '<span class="label label-info pull-right">']
-    menuu1 = menuu.replace(repl[i], ",abcdefghij,")
-
+    print(repl[i])
+    menuu = menuu.replace(repl[i], ",abcdefghij,")
 menuu = menuu.split(",abcdefghij,")
-
 menuud = []
-for i in range(1, len(menuu)-1, 2):
+for i in range(1, len(menuu), 2):
     menuud.append(menuu[i])
 hinnad_raw = soup.find_all("span", class_="label label-info pull-right")
 hinnad = []
 
 for hind in hinnad_raw:
-    hind.replace('<span class="label label-info pull-right">', " ")
-    hind.replace('</span>', " ")
-    hinnad.append(hind)
+    hind1 = str(hind)
+    hind2 = hind1.replace('<span class="label label-info pull-right">', "")
+    hind3 = hind2.replace('</span>', "")
+    hinnad.append(hind3)
 com = []
 kood_final = re.split("<small>|</small>", kood)
 for i in range(1, len(kood_final), 2):
@@ -39,18 +40,40 @@ with open("json mite enam kasitsi.py", "w") as json_fail:
 i = 0
 with open("json mite enam kasitsi.py", "a") as json_fail:
     for item in menuud:
-        if item == "PRAED" or item == "SUPID" or item == "MAGUSTOIDUD" or item == "JOOGID":
-            json_fail.write("   {")
-            json_fail.write('       "nimetus": ' + item + ",")
-            json_fail.write('       "toidud": [')
-        else:
-            json_fail.write("            {")
-            json_fail.write('                "nimetus": ' + item + ",")
-            json_fail.write('                "hind":' + hinnad[i] + ",")
-            json_fail.write('                "lisainfo":' + com[i] + ",")
-            i += 1
+        if item == "PRAED":
+            json_fail.write("\n   {")
+            json_fail.write('\n       "nimetus": "' + item + '",')
+            json_fail.write('\n       "toidud": [')
+        elif item == "SUPID" or item == "MAGUSTOIDUD" or item == "JOOGID":
+            json_fail.write("\n       ]")
+            json_fail.write("\n   },")
+            json_fail.write("\n   {")
+            json_fail.write('\n       "nimetus": "' + item + '",')
+            json_fail.write('\n       "toidud": [')
+        #elif (i<(len(menuud)-2)):
+         #   if menuud[i+1] == "SUPID" or item[i+1] == "MAGUSTOIDUD" or item[i+1] == "JOOGID":
+          #      json_fail.write("\n            {")
+           #     json_fail.write('\n                "nimetus": "' + item + '",')
+            #    json_fail.write('\n                "hind": "' + hinnad[i] + '",')
+             #   json_fail.write('\n                "lisainfo": "' + com[i] + '",')
+              #  json_fail.write("\n            }")
+               # i += 1
+            #else:
+             #   json_fail.write("\n            {")
+              #  json_fail.write('\n                "nimetus": "' + item + '",')
+               # json_fail.write('\n                "hind": "' + hinnad[i] + '",')
+                #json_fail.write('\n                "lisainfo": "' + com[i] + '",')
+                #json_fail.write("\n            },")
+                #i += 1
 
-print(hinnad)
-print(com)
-print(menuud)
-print(menuu)
+        else:
+            json_fail.write("\n            {")
+            json_fail.write('\n                "nimetus": "' + item + '",')
+            json_fail.write('\n                "hind": "' + hinnad[i] + '",')
+            json_fail.write('\n                "lisainfo": "' + com[i] + '",')
+            json_fail.write("\n            },")
+            i += 1
+    json_fail.write("\n      ]")
+    json_fail.write("\n   }")
+    json_fail.write("\n]")
+
